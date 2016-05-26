fb03fbf2d82dab2eb40552c0ad1c3b327fb32897
Noah Barney <nbarney@rsgc.on.ca>
2016-05-26 13:27:01 -0400

    Working on getting it to print the output

--- CompletionHandlersAndAsynchronousRequests.playground/Contents.swift
+++ CompletionHandlersAndAsynchronousRequests.playground/Contents.swift
@@ -16,14 +16,14 @@ class ViewController : UIViewController {
     // If data is successfully retrieved from the server, we can parse it here
     func parseMyJSON(theData : NSData) {
         
-//        // Print the provided data
-//        print("")
-//        print("====== the data provided to parseMyJSON is as follows ======")
-//        print(theData)
+        //        // Print the provided data
+        //        print("")
+        //        print("====== the data provided to parseMyJSON is as follows ======")
+        //        print(theData)
         
         // De-serializing JSON can throw errors, so should be inside a do-catch structure
         do {
-
+            
             // Do the initial de-serialization
             // Source JSON is here:
             // http://www.apilayer.net/api/live?access_key=eeda439c5824bfca1724944353882011&format=1
@@ -31,7 +31,7 @@ class ViewController : UIViewController {
             let json = try NSJSONSerialization.JSONObjectWithData(theData, options: NSJSONReadingOptions.AllowFragments)
             
             print(json)
-
+            
             print(json["quotes"])
             
             let quotes = json["quotes"]
@@ -39,213 +39,221 @@ class ViewController : UIViewController {
             if let asString = currencytextfield.text! as? String {
                 print("USD" + asString)
                 print(quotes!![("USD" + asString)])
-                var quote = String(quotes!![("USD" + asString)])
-                if let asDub = Double(moneytextfield.text!) {
-                    if let quoteasnum = Double(quote){
-                        print(quoteasnum*asDub)
-                    }
-                }
-            }
-        
-            
-            
-            // Now we can update the UI
-            // (must be done asynchronously)
-            dispatch_async(dispatch_get_main_queue()) {
-                self.jsonResult.text = "parsed JSON should go here"
-            }
-            
-        } catch let error as NSError {
-            print ("Failed to load: \(error.localizedDescription)")
-        }
-        
-        
-    }
-    
-    // Set up and begin an asynchronous request for JSON data
-    func getMyJSON() {
-
-        // Define a completion handler
-        // The completion handler is what gets called when this **asynchronous** network request is completed.
-        // This is where we'd process the JSON retrieved
-        let myCompletionHandler : (NSData?, NSURLResponse?, NSError?) -> Void = {
-            
-            (data, response, error) in
-            
-            // This is the code run when the network request completes
-            // When the request completes:
-            //
-            // data - contains the data from the request
-            // response - contains the HTTP response code(s)
-            // error - contains any error messages, if applicable
-            
-            // Cast the NSURLResponse object into an NSHTTPURLResponse objecct
-            if let r = response as? NSHTTPURLResponse {
-                
-                // If the request was successful, parse the given data
-                if r.statusCode == 200 {
-        
+                if let quote : Double = Double(String(quotes!![("USD" + asString)])) {
                     
-            
-                    if let d = data {
+                    print("contents of money field are \(moneytextfield.text!)")
+                    if let asDub = Double(moneytextfield.text!) {
+                        print("contents of money field as a Double are \(asDub)")
                         
-                        // Parse the retrieved data
-                        self.parseMyJSON(d)
-                        
+                        let convertedValue : Double = quote*asDub;
+                        print("conversion is:\(convertedValue)")
                     }
                     
+                } else {
+                    print("could not convert")
+                }
+            }
+            
+        
+        
+        // Now we can update the UI
+        // (must be done asynchronously)
+//        dispatch_async(dispatch_get_main_queue()) {
+//            self.jsonResult.text = "parsed JSON should go here"
+//        }
+        
+    } catch let error as NSError {
+    
+        print ("Failed to load: \(error.localizedDescription)")
+    
+    }
+    
+}
+
+// Set up and begin an asynchronous request for JSON data
+func getMyJSON() {
+    
+    // Define a completion handler
+    // The completion handler is what gets called when this **asynchronous** network request is completed.
+    // This is where we'd process the JSON retrieved
+    let myCompletionHandler : (NSData?, NSURLResponse?, NSError?) -> Void = {
+        
+        (data, response, error) in
+        
+        // This is the code run when the network request completes
+        // When the request completes:
+        //
+        // data - contains the data from the request
+        // response - contains the HTTP response code(s)
+        // error - contains any error messages, if applicable
+        
+        // Cast the NSURLResponse object into an NSHTTPURLResponse objecct
+        if let r = response as? NSHTTPURLResponse {
+            
+            // If the request was successful, parse the given data
+            if r.statusCode == 200 {
+                
+                
+                
+                if let d = data {
+                    
+                    // Parse the retrieved data
+                    self.parseMyJSON(d)
+                    
                 }
                 
             }
             
         }
         
-        // Define a URL to retrieve a JSON file from
-        let address : String = "http://www.apilayer.net/api/live?access_key=eeda439c5824bfca1724944353882011&format=1/"
+    }
+    
+    // Define a URL to retrieve a JSON file from
+    let address : String = "http://www.apilayer.net/api/live?access_key=eeda439c5824bfca1724944353882011&format=1/"
+    
+    // Try to make a URL request object
+    if let url = NSURL(string: address) {
         
-        // Try to make a URL request object
-        if let url = NSURL(string: address) {
-            
-            // We have an valid URL to work with
-            print(url)
-            
-            // Now we create a URL request object
-            let urlRequest = NSURLRequest(URL: url)
-            
-            // Now we need to create an NSURLSession object to send the request to the server
-            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: config)
-            
-            // Now we create the data task and specify the completion handler
-            let task = session.dataTaskWithRequest(urlRequest, completionHandler: myCompletionHandler)
-            
-            // Finally, we tell the task to start (despite the fact that the method is named "resume")
-            task.resume()
-            
-        } else {
-            
-            // The NSURL object could not be created
-            print("Error: Cannot create the NSURL object.")
-            
-        }
+        // We have an valid URL to work with
+        print(url)
+        
+        // Now we create a URL request object
+        let urlRequest = NSURLRequest(URL: url)
+        
+        // Now we need to create an NSURLSession object to send the request to the server
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        // Now we create the data task and specify the completion handler
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: myCompletionHandler)
+        
+        // Finally, we tell the task to start (despite the fact that the method is named "resume")
+        task.resume()
+        
+    } else {
+        
+        // The NSURL object could not be created
+        print("Error: Cannot create the NSURL object.")
         
     }
     
-    // This is the method that will run as soon as the view controller is created
-    override func viewDidLoad() {
-        
-        // Sub-classes of UIViewController must invoke the superclass method viewDidLoad in their
-        // own version of viewDidLoad()
-        super.viewDidLoad()
+}
 
-        // Make the view's background be gray
-        view.backgroundColor = UIColor.lightGrayColor()
-
-        /*
-         * Further define label that will show JSON data
-         */
-        
-        // Set the label text and appearance
-        jsonResult.text = "..."
-        jsonResult.font = UIFont.systemFontOfSize(12)
-        jsonResult.numberOfLines = 0   // makes number of lines dynamic
-        // e.g.: multiple lines will show up
-        jsonResult.textAlignment = NSTextAlignment.Center
-        
-        // Required to autolayout this label
-        jsonResult.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(jsonResult)
-
-        
-        moneytextfield.text = "$..."
-        moneytextfield.font = UIFont.systemFontOfSize(14)
-        moneytextfield.textAlignment = NSTextAlignment.Center
-        moneytextfield.borderStyle = .RoundedRect
-        moneytextfield.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview( moneytextfield)
-        
-        currencytextfield.text = "USDXXX"
-        currencytextfield.font = UIFont.systemFontOfSize(14)
-        currencytextfield.textAlignment = NSTextAlignment.Center
-        currencytextfield.borderStyle = .RoundedRect
-        currencytextfield.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(currencytextfield)
-        
-        USDlabel.text = "USD"
-        USDlabel.font = UIFont.systemFontOfSize(12)
-        USDlabel.numberOfLines = 0
-        USDlabel.textAlignment = NSTextAlignment.Center
-        USDlabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(USDlabel)
-        
-        DollarOutputLabel.text = "$output"
-        DollarOutputLabel.font = UIFont.systemFontOfSize(12)
-        DollarOutputLabel.numberOfLines = 0
-        DollarOutputLabel.textAlignment = NSTextAlignment.Center
-        DollarOutputLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(DollarOutputLabel)
-        
-        
-        
-        // Add the label to the superview
+// This is the method that will run as soon as the view controller is created
+override func viewDidLoad() {
     
-       
-        
-        
-        
-        /*
-         * Add a button
-         */
-        let getData = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 30))
-        
-        // Make the button, when touched, run the calculate method
-        getData.addTarget(self, action: #selector(ViewController.getMyJSON), forControlEvents: UIControlEvents.TouchUpInside)
-        
-        // Set the button's title
-        getData.setTitle("Get my JSON!", forState: UIControlState.Normal)
-        
-        // Required to auto layout this button
-        getData.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Add the button into the super view
-        view.addSubview(getData)
-
-        /*
-         * Layout all the interface elements
-         */
-        
-        // This is required to lay out the interface elements
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        // Create an empty list of constraints
-        var allConstraints = [NSLayoutConstraint]()
-        
-        // Create a dictionary of views that will be used in the layout constraints defined below
-        let viewsDictionary : [String : AnyObject] = [
-            "title": jsonResult,
-            "getData": getData,
-            "Ulab": USDlabel,
-            "CUtex":  currencytextfield,
-            "DOlab":  DollarOutputLabel,
-            "MOtex":  moneytextfield
-            ]
-        
-        // Define the vertical constraints
-        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|[title]-[MOtex][Ulab]-[getData]-[DOlab][CUtex]",
-            options: [],
-            metrics: nil,
-            views: viewsDictionary)
-        
-        // Add the vertical constraints to the list of constraints
-        allConstraints += verticalConstraints
-        
-        // Activate all defined constraints
-        NSLayoutConstraint.activateConstraints(allConstraints)
-        
-    }
+    // Sub-classes of UIViewController must invoke the superclass method viewDidLoad in their
+    // own version of viewDidLoad()
+    super.viewDidLoad()
     
+    // Make the view's background be gray
+    view.backgroundColor = UIColor.lightGrayColor()
+    
+    /*
+     * Further define label that will show JSON data
+     */
+    
+    // Set the label text and appearance
+    jsonResult.text = "..."
+    jsonResult.font = UIFont.systemFontOfSize(12)
+    jsonResult.numberOfLines = 0   // makes number of lines dynamic
+    // e.g.: multiple lines will show up
+    jsonResult.textAlignment = NSTextAlignment.Center
+    
+    // Required to autolayout this label
+    jsonResult.translatesAutoresizingMaskIntoConstraints = false
+    
+    view.addSubview(jsonResult)
+    
+    
+    moneytextfield.text = "$..."
+    moneytextfield.font = UIFont.systemFontOfSize(14)
+    moneytextfield.textAlignment = NSTextAlignment.Center
+    moneytextfield.borderStyle = .RoundedRect
+    moneytextfield.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview( moneytextfield)
+    
+    currencytextfield.text = "USDXXX"
+    currencytextfield.font = UIFont.systemFontOfSize(14)
+    currencytextfield.textAlignment = NSTextAlignment.Center
+    currencytextfield.borderStyle = .RoundedRect
+    currencytextfield.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(currencytextfield)
+    
+    USDlabel.text = "USD"
+    USDlabel.font = UIFont.systemFontOfSize(12)
+    USDlabel.numberOfLines = 0
+    USDlabel.textAlignment = NSTextAlignment.Center
+    USDlabel.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(USDlabel)
+    
+    DollarOutputLabel.text = "$output"
+    DollarOutputLabel.font = UIFont.systemFontOfSize(12)
+    DollarOutputLabel.numberOfLines = 0
+    DollarOutputLabel.textAlignment = NSTextAlignment.Center
+    DollarOutputLabel.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(DollarOutputLabel)
+    
+    
+    
+    // Add the label to the superview
+    
+    
+    
+    
+    
+    /*
+     * Add a button
+     */
+    let getData = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 30))
+    
+    // Make the button, when touched, run the calculate method
+    getData.addTarget(self, action: #selector(ViewController.getMyJSON), forControlEvents: UIControlEvents.TouchUpInside)
+    
+    // Set the button's title
+    getData.setTitle("Get my JSON!", forState: UIControlState.Normal)
+    
+    // Required to auto layout this button
+    getData.translatesAutoresizingMaskIntoConstraints = false
+    
+    // Add the button into the super view
+    view.addSubview(getData)
+    
+    /*
+     * Layout all the interface elements
+     */
+    
+    // This is required to lay out the interface elements
+    view.translatesAutoresizingMaskIntoConstraints = false
+    
+    // Create an empty list of constraints
+    var allConstraints = [NSLayoutConstraint]()
+    
+    // Create a dictionary of views that will be used in the layout constraints defined below
+    let viewsDictionary : [String : AnyObject] = [
+        "title": jsonResult,
+        "getData": getData,
+        "Ulab": USDlabel,
+        "CUtex":  currencytextfield,
+        "DOlab":  DollarOutputLabel,
+        "MOtex":  moneytextfield
+    ]
+    
+    // Define the vertical constraints
+    let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+        "V:|[title]-[MOtex][Ulab]-[getData]-[DOlab][CUtex]",
+        options: [],
+        metrics: nil,
+        views: viewsDictionary)
+    
+    // Add the vertical constraints to the list of constraints
+    allConstraints += verticalConstraints
+    
+    // Activate all defined constraints
+    NSLayoutConstraint.activateConstraints(allConstraints)
+    
+}
+
 }
 
 // Embed the view controller in the live view for the current playground page
